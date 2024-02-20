870bfb0bdd4615dbc78ceb6e293567be78de22b9
imon742 <nazmulislamemon007@gmail.com>
2024-02-20 23:12:12 +0600

    Update DiffrentWait.java

--- src/test/java/org/example/DiffrentWait.java
+++ src/test/java/org/example/DiffrentWait.java
@@ -13,19 +13,21 @@ public class DiffrentWait extends DriverSetup{
     @Test
     public void testDiffrentWait() throws InterruptedException {
 
-        browser.get("https://qavbox.github.io/demo/alerts/");
+
+
+        /*
 
         //heard sleep to stop java execution for fixed amount of time
 
         Thread.sleep(10000); //Static wait for stop execution(Fixed wait of java not selenium )
 
 
-        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//This is an implicit wait (Dynamic wait. Driver e ekbar manage kore dile jdi kuno exception pay seita solve hoy kina tar jnno wait korbe upto jotokhon ami bolbo)-> No such elements exception dbe.  
+        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//This is an implicit wait (Dynamic wait. Driver e ekbar manage kore dile jdi kuno exception pay seita solve hoy kina tar jnno wait korbe upto jotokhon ami bolbo)-> No such elements exception dbe.
 
 
         WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
 
-        browser.findElement(By.id("delayalert")).click();
+
 
         //heard sleep to stop java execution for fixed amount of time
 
@@ -33,10 +35,30 @@ public class DiffrentWait extends DriverSetup{
 
 
 
-        //Switching to alert and accept it  
+        //Switching to alert and accept it
 
         Alert alert = browser.switchTo().alert();
         alert.accept();
+
+         */
+
+        // Code section.
+
+        browser.get("https://qavbox.github.io/demo/alerts/");
+        browser.findElement(By.id("delayalert")).click();
+        Thread.sleep(10000);
+
+        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+
+
+        //Switching to alert and accept it
+
+        Alert alert = browser.switchTo().alert();
+        alert.accept();
+
+
 
     }
 }
