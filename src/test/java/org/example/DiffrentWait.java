d208fc0a6b956209d92ffa2eb4dbff34daf424fc
imon742 <nazmulislamemon007@gmail.com>
2024-02-20 11:19:39 +0600

    Update DiffrentWait.java

--- src/test/java/org/example/DiffrentWait.java
+++ src/test/java/org/example/DiffrentWait.java
@@ -2,7 +2,11 @@ package org.example;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class DiffrentWait extends DriverSetup{
 
@@ -10,6 +14,16 @@ public class DiffrentWait extends DriverSetup{
     public void testDiffrentWait() throws InterruptedException {
 
         browser.get("https://qavbox.github.io/demo/alerts/");
+
+        //heard sleep to stop java execution for fixed amount of time
+
+        Thread.sleep(10000); //Static wait for stop execution(Fixed wait of java not selenium )
+
+
+        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//This is an implicit wait (Dynamic wait. Driver e ekbar manage kore dile jdi kuno exception pay seita solve hoy kina tar jnno wait korbe upto jotokhon ami bolbo)-> No such elements exception dbe.  
+
+
+        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
 
         browser.findElement(By.id("delayalert")).click();
 
